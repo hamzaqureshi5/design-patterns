72f3def4b1bdc9c913c40417c2a13327dcdc58eb
Hamza Qureshi <hamza7771.861@gmail.com>
2025-10-08 15:45:40 +0500

    refactor

---
+++ singleton/build/CMakeFiles/pattern.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/pattern.dir/src/pattern.cpp.o"
+  "CMakeFiles/pattern.dir/src/pattern.cpp.o.d"
+  "pattern"
+  "pattern.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/pattern.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
